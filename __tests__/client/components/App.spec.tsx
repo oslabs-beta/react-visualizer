b6affdc1fe6f7f0bb00df7688582e5ffdcd068d7
michellesylee <michellebellelee@yahoo.com>
2023-02-23 17:38:38 -0800

    fixed hello from frontend test

--- __tests__/client/components/App.spec.tsx
+++ __tests__/client/components/App.spec.tsx
@@ -1,6 +1,6 @@
-/**
- * @jest-environment jsdom
- */
+// /**
+//  * @jest-environment jsdom
+//  */
 
 /* eslint-disable */
 // @ts-nocheck
@@ -8,11 +8,14 @@
 import { describe, expect, it } from '@jest/globals';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from '../../../src/client/components/App';
 
 describe('App', () => {
   it(`displays the 'Hello from the frontend!' text`, async () => {
     render(<App />);
+    const heading = screen.getByRole('heading');
+    expect(heading).toHaveTextContent('Hello from the frontend!');
     expect(screen.getByText('Hello from the frontend!')).toBeInTheDocument();
   });
 });
