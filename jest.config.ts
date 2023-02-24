45d31c8fa8ca1a192ef0afac7cd85933ee42f8e6
michellesylee <michellebellelee@yahoo.com>
2023-02-23 18:08:17 -0800

    fixed css with jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
     '^node_modules/(.*)$': '<rootDir>/node_modules/$1',
     '^src/(.*)$': '<rootDir>/src/$1',
   },
