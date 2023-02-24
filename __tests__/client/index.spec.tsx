d6856104d59ffa11b05c3caf11210f6a21365ba0
michellesylee <michellebellelee@yahoo.com>
2023-02-23 18:10:04 -0800

    working index test

--- __tests__/client/index.spec.tsx
+++ __tests__/client/index.spec.tsx
@@ -2,13 +2,7 @@
  * @jest-environment jsdom
  */
 
-/* eslint-disable */
-// @ts-nocheck
-
 import { describe, expect, it } from '@jest/globals';
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../../../src/client/components/App';
 
 describe('App', () => {
   it('renders the App component', () => {
