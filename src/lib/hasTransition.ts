f79214b13c3297d8b9261660c62d79db00b57061
PB-Liu <mindware2000@gmail.com>
2023-03-11 17:33:36 -0500

    migrated hasTransition function

--- src/lib/hasTransition.ts
+++ src/lib/hasTransition.ts
@@ -1,3 +1,7 @@
-const hasTransition = () => {};
+import { FiberLanes } from 'enums';
+
+const hasTransition = (laneNum: number): boolean =>
+  laneNum >= FiberLanes.TransitionHyrdationLane &&
+  laneNum <= FiberLanes.TransitionLane16;
 
 export default hasTransition;
